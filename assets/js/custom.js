74f9d8f7d9afdd9ad7301f59a524d69c7871d16e
unknown <gabakirti999@gmail.com>
2021-11-17 13:49:17 +0530

    header17

---
+++ assets/js/custom.js
@@ -0,0 +1,41 @@
+(function ($) {
+	
+	"use strict";
+
+	// Header Type = Fixed
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var box = $('.header-text').height();
+    var header = $('header').height();
+
+    if (scroll >= box - header) {
+      $("header").addClass("background-header");
+    } else {
+      $("header").removeClass("background-header");
+    }
+  });
+	// Menu Dropdown Toggle
+  if($('.menu-trigger').length){
+    $(".menu-trigger").on('click', function() { 
+      $(this).toggleClass('active');
+      $('.header-area .nav').slideToggle(200);
+    });
+  }
+
+	// Page loading animation
+	 $(window).on('load', function() {
+
+        $('#js-preloader').addClass('loaded');
+
+    });
+	// Window Resize Mobile Menu Fix
+  function mobileNav() {
+    var width = $(window).width();
+    $('.submenu').on('click', function() {
+      if(width < 767) {
+        $('.submenu ul').removeClass('active');
+        $(this).find('ul').toggleClass('active');
+      }
+    });
+  }
+})(window.jQuery);
